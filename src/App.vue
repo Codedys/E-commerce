b551f14fa9259d226e3d1371bf8fe4ca35dec3c3
Codedys <oberegeraldjackson@gmail.com>
2024-05-17 02:14:27 +0300

    Product card component added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 
+import ProductCard from './components/ProductCard.vue'
+
 </script>
 
 <template>
-  <header  class="bg-red-500 h-20 w-screen text-black flex justify-between items-center">
+  <header  class=" h-20 w-screen text-black flex justify-between items-center">
     <div class="flex space-x-4 ">
             <img class="ml-4" src="@/assets/icon-menu.svg" alt="burger" />
             <img src="@/assets/logo.svg" alt="logo"/>
@@ -13,14 +15,11 @@
             <img class="h-8 w-10 " src="@/assets/image-avatar.png" alt="avatar">
     </div> 
   </header>
-
   <main>
-    
-   
+    <ProductCard/>
   </main>
 </template>
 
+
 <style>
-
-
 </style>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+    <div class=" flex justify-between items-center bg-[url('@/assets/image-product-1-thumbnail.jpg')] h-64 w-screen bg-cover bg-center ">
+        <img class=" h-8 w-8 rounded-full ml-4 bg-white" src="@/assets/icon-previous.svg" alt="previous">
+        <img class=" h-8 w-8 rounded-full mr-4 bg-white" src="@/assets/icon-next.svg" alt="next">
+    </div>
+</template>
