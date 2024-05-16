857b4af0de7f9ef7169773a2806665429866a845
Codedys <oberegeraldjackson@gmail.com>
2024-05-17 01:32:24 +0300

    mobile navigation

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,26 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <header  class="bg-red-500 h-20 w-screen text-black flex justify-between items-center">
+    <div class="flex space-x-4 ">
+            <img class="ml-4" src="@/assets/icon-menu.svg" alt="burger" />
+            <img src="@/assets/logo.svg" alt="logo"/>
     </div>
+    <div class="flex space-x-4 mr-4 ">
+            <img src="@/assets/icon-cart.svg" alt="cat">
+            <img class="h-8 w-10 " src="@/assets/image-avatar.png" alt="avatar">
+    </div> 
   </header>
 
   <main>
-    <TheWelcome />
+    
+   
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<style>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
